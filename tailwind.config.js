b5a82738f9f96a222fc62146ce5b4dea5cadbc63
Serverguyken <serverguyken@gmail.com>
2021-05-21 17:11:09 -0400

    changed build script

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  purge: [
-    "./pages/**/*.vue", 
-    "./components/**/*.vue", 
-    "./plugins/**/*.vue",
-    "./static/**/*.vue",
-    "./store/**/*.vue"
-  ],
+  purge: ["./dist/**/*.html", "./src/**/*.vue"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
